b71950846d800acf21b9693300358ff878b56583
Keegan Skeate <keeganskeate@gmail.com>
2016-07-18 18:55:31 -0700

    Update init.js

--- javascripts/init.js
+++ javascripts/init.js
@@ -84,7 +84,7 @@
 		
 		//Connected Hover on image and title
 		$(function() {
-  $('#footer .major h3').hover(function() {
+  $('#footer .subject h3').hover(function() {
     $('#footer .images').css('border-color', '#235796');
   }, function() {
     // on mouseout, reset the background colour
