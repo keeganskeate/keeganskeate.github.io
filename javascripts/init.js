2ff64973618ef9da4c1e22cbced28a2c933b5165
Keegan Skeate <keeganskeate@gmail.com>
2016-07-22 17:26:56 -0700

    Update init.js

--- javascripts/init.js
+++ javascripts/init.js
@@ -93,11 +93,26 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 
 window.onscroll = function() {myPopup()};
 function myPopup() {
     if (document.body.scrollTop > 50 ) {
-        document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementById("myDropdown").classList.add("show");
     } 
 }
 
