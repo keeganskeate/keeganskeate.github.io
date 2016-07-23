bcf3185c1e4476993c04549797c8b0a8f5fec7da
Keegan Skeate <keeganskeate@gmail.com>
2016-07-22 17:19:24 -0700

    Update init.js

--- javascripts/init.js
+++ javascripts/init.js
@@ -111,15 +111,7 @@ window.onclick = function(event) {
 window.onscroll = function() {myPopup()};
 function myPopup() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        ocument.getElementById("myDropdown").classList.toggle("show");
-    } else {
-       var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }}
-    }
+        document.getElementById("myDropdown").classList.toggle("show");
+    } 
 }
 
