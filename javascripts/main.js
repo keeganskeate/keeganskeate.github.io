573beab24a3b9abac41f66c5eb781228face4089
Keegan Skeate <keeganskeate@gmail.com>
2018-04-12 06:19:27 -0700

    removed back-to-top button script

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,21 +16,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-// Back-to-top Button
-jQuery(document).ready(function() {
-	var offset = 250; // The offset is when the button will appears.
-	var duration = 300; // The duration is the time until the button will appear in milliseconds.
-	jQuery(window).scroll(function() {
-		if (jQuery(this).scrollTop() > offset) {
-			jQuery('.back-to-top').fadeIn(duration);
-		} else {
-			jQuery('.back-to-top').fadeOut(duration);
-		}
-	});
-	jQuery('.back-to-top').click(function(event) {
-		event.preventDefault();
-		jQuery('html, body').animate({scrollTop: 0}, duration);
-		return false;
-	});
-});
