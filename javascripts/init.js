273dc0dc90bbae586ad5570747d3a9ebf4c09954
Keegan Skeate <keeganskeate@gmail.com>
2018-04-12 06:20:32 -0700

    added back-to-top button script

--- javascripts/init.js
+++ javascripts/init.js
@@ -1,4 +1,4 @@
-/* Original template by TEMPLATED (templated.co) */
+/* Template */
 (function($) {
 
 	skel.init({
@@ -80,3 +80,21 @@
 		$(document).tooltip({show: null});
 
 });})(jQuery);
+
+// Back-to-top Button
+jQuery(document).ready(function() {
+	var offset = 250; // The offset is when the button will appears.
+	var duration = 300; // The duration is the time until the button will appear in milliseconds.
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > offset) {
+			jQuery('.back-to-top').fadeIn(duration);
+		} else {
+			jQuery('.back-to-top').fadeOut(duration);
+		}
+	});
+	jQuery('.back-to-top').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({scrollTop: 0}, duration);
+		return false;
+	});
+});
