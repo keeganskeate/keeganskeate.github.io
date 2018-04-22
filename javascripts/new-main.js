01f0f154ba7ccf988797c5d14d31dc76cc1057b8
Keegan Skeate <keeganskeate@gmail.com>
2018-04-22 05:22:41 -0700

    full init template js

--- javascripts/new-main.js
+++ javascripts/new-main.js
@@ -6,12 +6,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global:		{ range: '*', href: '/stylesheets/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: '/stylesheets/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-			normal:		{ range: '-1280', href: '/stylesheets/style-normal.css', containers: 960, lockViewport: true },
-			narrow:		{ range: '-1028', href: '/stylesheets/style-narrow.css', containers: '70%', grid: { gutters: 25 } },
-			narrower:	{ range: '-980', href: '/stylesheets/style-narrower.css', grid: { collapse: true } },
-			mobile:		{ range: '-640', href: '/stylesheets/style-mobile.css', containers: '70%', grid: { gutters: 15 } }
+			global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
+			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+			normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, lockViewport: true },
+			narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 25 } },
+			narrower:	{ range: '-840', href: 'css/style-narrower.css', grid: { collapse: true } },
+			mobile:		{ range: '-640', href: 'css/style-mobile.css', containers: '90%', grid: { gutters: 15 } }
 		}
 	}, {
 		layers: {
@@ -26,12 +26,12 @@
 					orientation: 'vertical',
 					position: 'top-left',
 					side: 'left',
-					width: 400
+					width: 275
 				},
 				titleBar: {
 					breakpoints: 'narrower',
-					height: 50,
-					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="mobile-logo"></span>',
+					height: 44,
+					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
 					position: 'top-left',
 					side: 'top',
 					width: '100%'
@@ -39,7 +39,19 @@
 			}
 		}
 	});
-		
+	
+	// Sticky header with slide down effect
+	$(function() {
+		$("#header").before($(".StickyHeader").clone().addClass("fixed"));
+		$(window).scroll(function(){
+			if ($(window).scrollTop() >= 200) {
+				$('.StickyHeader.fixed').addClass('slideDown');
+			} else{
+				$('.StickyHeader.fixed').removeClass('slideDown');
+			}
+		});
+	});
+
 	// Disable animations/transitions until page has loaded.
 	$(function() {
 		var	$window = $(window),
@@ -50,11 +62,19 @@
 			});
 	});
 	
+	// Initiate Dropdowns.
+	$(function() {
+		$('#nav > ul').dropotron({
+			offsetY: -15,
+			hoverDelay: 0
+		});
+	});
+	
 })(jQuery);
   
-// Back-to-top Button
+/* Back-to-top Button */
 jQuery(document).ready(function() {
-  var offset = 250; // When the button will appear.
+  var offset = 100; // When the button will appear.
   var duration = 300; // The time until the button will appear in milliseconds.
   jQuery(window).scroll(function() {
     if (jQuery(this).scrollTop() > offset) {
